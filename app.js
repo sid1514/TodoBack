c27a8af6bcb333451ca8577c01f1f36a37347b0b
Siddharth Santosh Shinde <142812176+sid1514@users.noreply.github.com>
2024-02-03 14:08:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const express=require("express")
+const route=require("./route.js")
+const cors=require("cors")
+const app=express()
+const dotenv=require("dotenv")
+dotenv.config({path:"./secret.env"});
+
+const port=process.env.port;
+
+app.use(express.json())
+app.use(cors())
+app.use(route)
+
+app.listen(port,()=>{
+    console.log("server on")
+})
+
+
+
---
+++ conn.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose")
+const dotenv=require("dotenv")
+
+
+dotenv.config({path:"./secret.env"})
+const db=process.env.dbs
+
+
+
+mongoose.connect(db,{
+   
+   useNewUrlParser:true,
+   useUnifiedTopology:true
+})
+.then(()=>console.log("connected with database"))
+.catch((e)=>console.log(e))
